e6d7448e5a957cdac54712c517a4f6c700e03628
Temur Chitashvili <temur.chitashvilii@gmail.com>
2024-11-06 22:21:03 +0400

    initial commit

---
+++ FinMate/FinMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinMateApp.swift
+//  FinMate
+//
+//  Created by Temur Chitashvili on 06.11.24.
+//
+
+import SwiftUI
+
+@main
+struct FinMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
